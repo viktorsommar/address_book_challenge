1bc8364e46dacb2973d2773c5a2c202e2baf2dc1
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-03 16:00:07 +0200

    Adding save contact button, test passed.

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -8,5 +8,14 @@ describe('user can create a contact', () => {
         cy.get('#company').type('Craft Academy')
         cy.get('#notes').type('Awesome coder')
         cy.get('#twitter').type('@thomasochman')
+        cy.get('#submit').click()
+    })
+
+    it('displays a name of the new contact', () => {
+        cy.get('#contact-list').should('contain', 'Thomas')
+    })
+
+    it('displays the phone number of the new contact', () => {
+        cy.get('#contact-list').should('contain', '0700101010')
     })
 })
